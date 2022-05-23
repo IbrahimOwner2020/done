9814fc421852ba62d6efb40b69d3f8f1eeffb690
IbrahimOwner2020 <kidibra2020@gmail.com>
2022-05-23 15:04:39 +0300

    setups

---
+++ firebase/config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDbo-dsNSO_2S1IoBx7rGmRL7qiQ_5g8CQ",
+	authDomain: "todo-d957f.firebaseapp.com",
+	projectId: "todo-d957f",
+	storageBucket: "todo-d957f.appspot.com",
+	messagingSenderId: "218469255640",
+	appId: "1:218469255640:web:f1c90b9c21968ff30f0aad",
+	measurementId: "G-DF14V8WRMG",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
