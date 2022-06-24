840cf86535681a6234974a30796d0b6817eca590
IbrahimOwner2020 <kidibra2020@gmail.com>
2022-06-24 10:52:59 +0300

    feat: hide-env-variables

--- firebase/config.ts
+++ firebase/config.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDbo-dsNSO_2S1IoBx7rGmRL7qiQ_5g8CQ",
-	authDomain: "todo-d957f.firebaseapp.com",
-	projectId: "todo-d957f",
-	storageBucket: "todo-d957f.appspot.com",
-	messagingSenderId: "218469255640",
-	appId: "1:218469255640:web:f1c90b9c21968ff30f0aad",
-	measurementId: "G-DF14V8WRMG",
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
 };
 
 const app = initializeApp(firebaseConfig);
--- firebase/firebaseAdmin.ts
+++ firebase/firebaseAdmin.ts
@@ -1,9 +1,12 @@
 import admin from "firebase-admin";
-const serviceAccount = require("./todo-serviceAccount.json");
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+        projectId: process.env.NEXT_PUBLIC_FIREBASEADMIN_PROJECTID,
+        clientEmail: process.env.NEXT_PUBLIC_FIREBASEADMIN_CLIENTEMAIL,
+        privateKey: process.env.NEXT_PUBLIC_FIREBASEADMIN_PRIVATEKEY?.replace(/\\n/g, '\n'),
+    }),
   });
 }
 
